cde83645192929d26587d2c6d16230c2e6dd9ad3
Kevin <huanqing19901001@163.com>
2018-08-21 23:54:22 +0800

    Modified case04, 'outputs' as 'output'.

--- wellgroundedrubyst/chapter01/case04_c2fout.rb
+++ wellgroundedrubyst/chapter01/case04_c2fout.rb
@@ -1,7 +1,7 @@
 print "Hello. Please enter a Celsius value: "
 celsius = gets.to_i
 fahrenheit = (celsius * 9 / 5) + 32
-puts "Saving result to outputs file 'temp.out'..."
+puts "Saving result to output file 'temp.out'..."
 fh = File.new("H:\\codelearning\\wellgroundedrubist\\chapter01\\temp.out", "w")
 fh.puts(fahrenheit.to_s)
 fh.close
