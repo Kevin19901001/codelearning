d45ab28d6b7d817d07812b2d078d9a7f8ebf6d8e
Kevin <huanqing19901001@163.com>
2018-09-13 23:21:01 +0800

    Modified case03.

--- wellgroundedrubyst/chapter02/case03_object_behaviours.rb
+++ wellgroundedrubyst/chapter02/case03_object_behaviours.rb
@@ -4,3 +4,6 @@ p Object.new.methods.sort
 # object_id
 obj = Object.new
 puts "The id of obj is #{obj.object_id}."
+
+str = "Strings are objects too, and this is a string!"
+puts "The id of string object str is #{str.object_id}."
