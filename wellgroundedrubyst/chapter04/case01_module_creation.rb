422b9beca41c3bc26aa7807d9fdc0b32ed5916c6
Kevin <huanqing19901001@163.com>
2018-10-17 23:53:35 +0800

    Some modifier.

--- wellgroundedrubyst/chapter04/case01_module_creation.rb
+++ wellgroundedrubyst/chapter04/case01_module_creation.rb
@@ -1,12 +1,26 @@
 module MyFirstModule
-  def say_hello
-    puts "hello"
-  end
+	def say_hello
+		puts "hello"
+	end
 end
 
 class ModuleTester
-  include MyFirstModule
+	include MyFirstModule
 end
 
 mt = ModuleTester.new
 mt.say_hello()
+
+module Stacklike
+	def stack
+		@stack |= []
+	end
+
+	def add_to_stack(obj)
+		stack << obj
+	end
+
+	def take_from_stack
+		stack.pop
+	end
+end
