92f0cbdd9680746067aa4f808822937a330a83a5
Kevin <huanqing19901001@163.com>
2018-10-29 23:05:57 +0800

    Add the first file for unit05.

---
+++ wellgroundedrubyst/chapter05/case01_self_comprehension.rb
@@ -0,0 +1,24 @@
+# The object which transformed into 'self'.
+
+puts "Top Level"
+puts "self is #{self}"
+
+class C
+
+  puts "Class definition block:"
+  puts "self is #{self}"
+
+  def self.x
+    puts "Class method C.x:"
+    puts "self is #{self}"
+  end
+
+  def m
+    puts "Instance method C#m:"
+    puts "self is #{self}"
+  end
+
+end
+
+C.x
+C.new.m
