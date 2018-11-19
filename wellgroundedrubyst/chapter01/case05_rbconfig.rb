ef3cc8ba05533193ac9076aadf222d636d512caa
kevin <huanqing19901001@163.com>
2018-11-19 21:50:25 +0800

    Some changes.

--- wellgroundedrubyst/chapter01/case05_rbconfig.rb
+++ wellgroundedrubyst/chapter01/case05_rbconfig.rb
@@ -9,14 +9,14 @@ puts RbConfig::CONFIG["rubylibdir"]
 # Special architecture（compiled，binary files）
 puts RbConfig::CONFIG["archdir"]
   
-# User own or third extensions lib files（written by Ruby）:
+# User's own or third extensions lib files（written by Ruby）:
 puts RbConfig::CONFIG["sitedir"]
   
 # Third extensions and lib files(witten by Ruby):
 puts RbConfig::CONFIG["vendordir"]
   
-# User own Ruby language extensions(written by Ruby）:
+# User's own Ruby language extensions(written by Ruby）:
 puts RbConfig::CONFIG["sitelibdir"]
   
-# User own Ruby language extensions(written by C):
+# User's own Ruby language extensions(written by C):
 puts RbConfig::CONFIG["sitearchdir"]
