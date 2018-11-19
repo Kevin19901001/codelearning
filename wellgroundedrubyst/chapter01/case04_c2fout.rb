ce977e574fed7ae9c7d61fbf760b53e27d814a19
kevin <huanqing19901001@163.com>
2018-11-19 21:48:31 +0800

    Some changes.

--- wellgroundedrubyst/chapter01/case04_c2fout.rb
+++ wellgroundedrubyst/chapter01/case04_c2fout.rb
@@ -2,7 +2,8 @@ print "Hello. Please enter a Celsius value: "
 celsius = gets.to_i
 fahrenheit = (celsius * 9 / 5) + 32
 puts "Saving result to output file 'temp.out'..."
-fh = File.new("H:\\codelearning\\wellgroundedrubist\\chapter01\\temp.out", "w")
+# fh = File.new("H:\\codelearning\\wellgroundedrubist\\chapter01\\temp.out", "w")	# In windows
+fh = File.new("temp.out", "w")								# In linux
 fh.puts(fahrenheit.to_s)
 fh.close
 puts "Finished!"
