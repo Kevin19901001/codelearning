5ab547750fe54e91ce78cfcd4cade49b0c500673
kevin <huanqing19901001@163.com>
2018-11-21 22:08:50 +0800

    Some changes.

--- wellgroundedrubyst/chapter05/case03_access_rules.rb
+++ wellgroundedrubyst/chapter05/case03_access_rules.rb
@@ -22,10 +22,12 @@ class Baker
 
   def pour_flour
     @batter.push(Flour.new)
+    puts "Flour has been poured!"
   end
 
   def add_egg
     @batter.push(Egg.new)
+    puts "Eggs have been added!"
   end
 
   def stir_batter
@@ -36,4 +38,4 @@ end
 
 b = Baker.new
 #b.add_egg	# private method `add_egg' called for #<Baker:0x007fd6e11893d0> (NoMethodError)
-b.bake_cake
+b.bake_cake	# Flour has been poured! Eggs have been added!
