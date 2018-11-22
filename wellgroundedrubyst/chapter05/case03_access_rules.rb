37a5c8a628a8b6c3c9ef7ac6ba6fd3b65f0b03ce
kevin <huanqing19901001@163.com>
2018-11-22 22:19:15 +0800

    Some changes.

--- wellgroundedrubyst/chapter05/case03_access_rules.rb
+++ wellgroundedrubyst/chapter05/case03_access_rules.rb
@@ -64,3 +64,23 @@ puts "Rover is #{rover.dog_years} in dog years."
 
 
 # Protected methods:
+class C
+  def initialize(n)
+    @n = n
+  end
+
+  def n
+    @n
+  end
+
+  def compare(c)
+    if c.n > n
+      puts "The other object's n is bigger."
+    else
+      puts "The other object's n is the same or smaller."
+    end
+  end
+
+  protected :n
+end
+
