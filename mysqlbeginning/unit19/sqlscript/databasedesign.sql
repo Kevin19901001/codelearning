2abc6d8736ac49a981ecb36566bfc9f00d0f6ce5
Kevin <huanqing19901001@163.com>
2018-11-24 22:59:27 +0800

    Some changes.

--- mysqlbeginning/unit19/sqlscript/databasedesign.sql
+++ mysqlbeginning/unit19/sqlscript/databasedesign.sql
@@ -1,7 +1,7 @@
 create table book(
 	book_id 	int auto_increment,
 	name 		varchar(100) not null,
-	sn 			varchar(100) unique not null,
+	sn 		varchar(100) unique not null,
 	price 		decimal(5, 2) not null default 0,
 	remark 		varchar(100),
 	primary key(book_id)
@@ -23,7 +23,7 @@ create table book_order(
 create table order_item(
 	order_id		varchar(50) not null,
 	book_id			int not null,
-	num				int not null,
+	num			int not null,
 	primary key		(order_id, book_id),
 	foreign key		(book_id) references book(book_id) on delete cascade on update cascade,
 	foreign key		(order_id) references book_order(order_id) on delete cascade on update cascade
