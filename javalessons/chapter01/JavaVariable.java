47ac439e8b812ff773a05a1f430a2f87292f09a6
kevin <huanqing19901001@163.com>
2018-11-27 21:16:12 +0800

    Variable introduction.

---
+++ javalessons/chapter01/JavaVariable.java
@@ -0,0 +1,21 @@
+// Java variable:
+public class JavaVariable {
+    // What is variable:
+    int a = 5;
+    int b = 6;
+    int c = a + b;
+
+    // Variable declaration:
+    public static void main(String[] args){
+        //a = 1;                      // Syntax error, an undeclared variable can not be used!
+        int score = 100;
+        //scord = 100;                // Syntax error, scord has not been declared!
+        System.out.println(score);
+    }
+
+    //Init variable:
+
+
+    // Access variable:
+
+}
