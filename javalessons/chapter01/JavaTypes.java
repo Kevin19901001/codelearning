4bebe9c262831b768b5ba0fb8dabbf3e0289bfc6
kevin <huanqing19901001@163.com>
2018-11-28 23:08:04 +0800

    Add a file.

---
+++ javalessons/chapter01/JavaTypes.java
@@ -0,0 +1,17 @@
+public class JavaTypes {
+//		    Java basic data type
+//			        |
+//			        |
+//    	  Integer            Float        char        boolean
+//            |		        |
+//            |		        |
+//  byte short int long    float  double
+
+
+    // int type:
+    // (-2^21) ~ (2^31-1)
+    int a = 100;		// Decimal.
+    int b = 0x186a0;		// Hex.
+    int c = 0303240;		// Octal.
+    //int d = 10000000000;	// Syntax error! Too big Integer!
+}
