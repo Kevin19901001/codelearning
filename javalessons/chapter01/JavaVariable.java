edf7c6b63449298fb0224ccb23fc13e98d057858
kevin <huanqing19901001@163.com>
2018-11-28 22:00:28 +0800

    Some changes.

--- javalessons/chapter01/JavaVariable.java
+++ javalessons/chapter01/JavaVariable.java
@@ -7,20 +7,29 @@ public class JavaVariable {
 
     // Variable declaration:
     public static void main(String[] args){
-        //a = 1;                          // Syntax error, an undeclared variable can not be used!
+        //d = 1;                          // Syntax error, an undeclared variable can not be used!
         int score = 100;
         //scord = 100;                    // Syntax error, scord has not been declared!
         System.out.println(score);
         
-        int a = 1, b = 2;	          // Declare two variables 1 and 2.
-        int c,d = 3;
-        System.out.println("a: " + a);    // a: 1.
-        System.out.println("b: " + b);    // b: 2.
-        //System.out.println("c: " + c);  // Syntax error, c has not been declared!
-        System.out.println("d: " + d);    // c: 3.
-    }
+        int e = 1, f = 2;	          // Declare two variables 1 and 2.
+        int g,h = 3;
+        System.out.println("e: " + e);    // e: 1.
+        System.out.println("f: " + f);    // f: 2.
+        //System.out.println("g: " + g);  // Syntax error, g has not been declared!
+        System.out.println("h: " + h);    // h: 3.
 
-    //Init variable:
+
+        // Variable initialization:
+        int x, y = 10;                    // This declaration just inited 'y', but 'x' has not been
+        //int z = x + y;	  	  // Syntax error!
+        
+        int sum = 0;
+        int m = 5;
+        int n = 6;
+        sum = m + n;
+        System.out.println("sum : " + sum);	// sum : 11.
+    }
 
 
     // Access variable:
