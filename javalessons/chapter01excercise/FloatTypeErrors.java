d12183a84b458b94b9e3c94d700c2fef49ccb57e
kevin <huanqing19901001@163.com>
2018-12-07 22:02:22 +0800

    Add a file.

---
+++ javalessons/chapter01excercise/FloatTypeErrors.java
@@ -0,0 +1,24 @@
+package chapter01excercise;
+
+/**
+ * 
+ * @description Float type errors
+ * @author kevin
+ * @since 2018/12/06
+ *
+ */
+public class FloatTypeErrors {
+
+	/**
+	 * 
+	 * @description Float type errors
+	 * @author kevin
+	 * @param args
+	 * @since 2018/12/06
+	 * 
+	 */
+	public static void main(String[] args) {
+		
+	}
+
+}
