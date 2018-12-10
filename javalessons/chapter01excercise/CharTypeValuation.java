c1c31f04d27e32e8077dae0905fa2474dd8b9ec1
kevin <huanqing19901001@163.com>
2018-12-10 21:20:18 +0800

    Add a file.

---
+++ javalessons/chapter01excercise/CharTypeValuation.java
@@ -0,0 +1,30 @@
+package chapter01excercise;
+
+/**
+ * 
+ * @description Char type
+ * @author kevin
+ * @since 2018/12/10
+ *
+ */
+
+    public class CharTypeValuation{
+
+        /**
+         *
+         * @author kevin
+         * @para args
+         * @since 2018/12/10
+         *
+         */
+        public static void main(String[] args){
+
+            char zhong = '中';
+            int zhongValue = zhong;
+            System.out.println(zhongValue);        // 20013.
+
+            char c = 97;
+            System.out.println(c);                 // a.
+
+        }
+  }
