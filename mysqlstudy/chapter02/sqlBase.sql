7564077e4acada65143a71de590f1d3bb0e7e85c
kevin <huanqing19901001@163.com>
2018-12-11 22:16:58 +0800

    Add a file.

---
+++ mysqlstudy/chapter02/sqlBase.sql
@@ -0,0 +1,13 @@
+// Create a database:
+// CREATE DATABASE dbname;
+create database test1;        // Create a database 'test1'.
+show databases;               // Query all databaes in database system.
+use test1;                    // Use databse 'test1'.
+show tables;                  // Quesy all tables in database 'test1'.
+use mysql;                    // Use database 'mysql'.
+show tables;                  // There are many tables in 'mysql'.
+
+// Delete a database:
+// drop database dbname;
+drop database test1;          // Delete database 'test1'.
+show databases;               // There is no database named 'test1' because it has been deleted.
