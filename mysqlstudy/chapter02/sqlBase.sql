6140e8810eac54d5eb528299011f86dda4aa58ce
kevin <huanqing19901001@163.com>
2018-12-13 23:00:36 +0800

    Add some content.

--- mysqlstudy/chapter02/sqlBase.sql
+++ mysqlstudy/chapter02/sqlBase.sql
@@ -11,3 +11,19 @@ show tables;                  // There are many tables in 'mysql'.
 // drop database dbname;
 drop database test1;          // Delete database 'test1'.
 show databases;               // There is no database named 'test1' because it has been deleted.
+
+// Create table:
+// create table tablename(
+//   column_name_1 column_type_1 constraints,
+//   column_name_2 column_type_2 constraints,
+//   ...
+//   column_name_n column_type_n constraints
+// );
+create table emp(
+  ename         varchar(10),
+  hiredate      date,
+  sal           decimal(10,2),
+  deptnp        int(2)
+);
+show tables;                  // show tables in database 'test1'. There is 'emp'.
+desc emp;                     // Show the detail of table 'emp'.
