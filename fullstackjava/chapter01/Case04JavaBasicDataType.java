d71193cb2da1c629da1317c6884726b7b7c800f8
FanHuanQing <huanqing19901001@163.com>
2020-01-02 22:42:03 +0800

    Add some content

--- fullstackjava/chapter01/Case04JavaBasicDataType.java
+++ fullstackjava/chapter01/Case04JavaBasicDataType.java
@@ -3,8 +3,25 @@ package chapter01;
 public class Case04JavaBasicDataType {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		// There are 8 kinds of data type in java:
+		
+		// int: -2^31 ~ 2^31
+		int a = 100000;
+		int b = 0x168a0;
+		int c = 0303240;
+		System.out.println(a + b + c);
+		
+		int i = 5/3;
+		System.out.println(i);
+		
+		int total = 87;
+		int error = 23;
+		int percent = error/total*100;
+		System.out.println(percent);
+		
+		percent = 100*error/total;
+		System.out.println(percent + "%");
 	}
 
 }
