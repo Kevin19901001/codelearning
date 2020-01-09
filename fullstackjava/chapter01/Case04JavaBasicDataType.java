11760e0e18a32e42d56d568418f1718cff7c9356
FanHuanQing <huanqing19901001@163.com>
2020-01-09 22:55:48 +0800

    Some new content

--- fullstackjava/chapter01/Case04JavaBasicDataType.java
+++ fullstackjava/chapter01/Case04JavaBasicDataType.java
@@ -94,6 +94,16 @@ public class Case04JavaBasicDataType {
 		
 		double persent2 = 80.0/100;
 		System.out.println(persent2);						// 0.8
+		
+		// %:
+		int n = 225;
+		System.out.println(n%8);							// 1
+		
+		// ++ and --:
+		int aa = 10, bb = 20;
+		int cc1 = aa++;
+		int cc2 = ++bb;
+		System.out.println("aa:" + aa + " bb:" + bb + " cc1:" + cc1 + " cc2:" + cc2);	// aa:11 bb:21 cc1:10 cc2:21
 	}
 
 }
