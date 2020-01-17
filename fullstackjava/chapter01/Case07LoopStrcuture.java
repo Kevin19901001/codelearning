ab64e6740a2108f1bed676bafad220bcc4462bef
FanHuanQing <huanqing19901001@163.com>
2020-01-17 22:53:07 +0800

    Some new content for loop structure

---
+++ fullstackjava/chapter01/Case07LoopStrcuture.java
@@ -0,0 +1,19 @@
+package chapter01;
+
+public class Case07LoopStrcuture {
+
+	public static void main(String[] args) {
+		// while:
+		int age = 1;
+		while (age < 100) {
+			System.out.println("Money!Money!Money!");
+			age++;
+		}
+		
+		// do...while...
+		
+		// for:
+		
+	}
+
+}
