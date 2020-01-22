8fa4b19892717ca2a347879d1f5380a44c9f301c
FanHuanQing <huanqing19901001@163.com>
2020-01-22 22:13:37 +0800

    Create an array

--- fullstackjava/chapter01/Case08ArrayIntroduction.java
+++ fullstackjava/chapter01/Case08ArrayIntroduction.java
@@ -3,8 +3,8 @@ package chapter01;
 public class Case08ArrayIntroduction {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] arr = {1, 3, 5, 7, 2, 8, 6, 4, 2};
+		System.out.println(arr);
 	}
 
 }
