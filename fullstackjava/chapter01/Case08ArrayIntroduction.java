1300a23696426c701476d74f8bee43cc1bd2ab27
FanHuanQing <huanqing19901001@163.com>
2020-01-23 18:08:23 +0800

    Some new content about array

--- fullstackjava/chapter01/Case08ArrayIntroduction.java
+++ fullstackjava/chapter01/Case08ArrayIntroduction.java
@@ -5,6 +5,18 @@ public class Case08ArrayIntroduction {
 	public static void main(String[] args) {
 		int[] arr = {1, 3, 5, 7, 2, 8, 6, 4, 2};
 		System.out.println(arr);
+		
+		// initialize array:
+		
+		// length of array:
+		
+		// access array through index:
+		
+		// iterate elements from array:
+		
+		// copy array:
+		
+		// sort array:
 	}
 
 }
