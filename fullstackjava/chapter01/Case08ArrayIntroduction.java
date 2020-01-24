8236e2f952aae88e51df455eed432c9c3f7fbfd3
FanHuanQing <huanqing19901001@163.com>
2020-01-24 22:25:15 +0800

    Some new content about length of array

--- fullstackjava/chapter01/Case08ArrayIntroduction.java
+++ fullstackjava/chapter01/Case08ArrayIntroduction.java
@@ -3,12 +3,19 @@ package chapter01;
 public class Case08ArrayIntroduction {
 
 	public static void main(String[] args) {
-		int[] arr = {1, 3, 5, 7, 2, 8, 6, 4, 2};
-		System.out.println(arr);
 		
 		// initialize array:
+		int[] arr1 = {1, 3, 5, 7, 2, 8, 6, 4, 2};
+		System.out.println(arr1);
+		
+		int[] arr2;
+		arr2 = new int[]{10, 23, 30, -10, 96};
+		System.out.println(arr2);
 		
 		// length of array:
+		int[] arr3 = new int[] {3, 6, 8, 9};
+		int length = arr3.length;
+		System.out.println("数组的长度为：" + length);
 		
 		// access array through index:
 		
