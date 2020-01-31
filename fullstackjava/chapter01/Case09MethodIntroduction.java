d781fc92f1b546a005ba3157b2f5dde4e475f014
FanHuanQing <huanqing19901001@163.com>
2020-01-31 21:56:08 +0800

    Add a file

---
+++ fullstackjava/chapter01/Case09MethodIntroduction.java
@@ -0,0 +1,13 @@
+package chapter01;
+
+public class Case09MethodIntroduction {
+
+	public static void main(String[] args) {
+		// method:
+		
+		// method definition:
+		
+		// method calling:
+	}
+
+}
