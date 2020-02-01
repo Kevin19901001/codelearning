a86542548c48a0020cbaebc37c1dd8abf46b4823
FanHuanQing <huanqing19901001@163.com>
2020-02-01 20:09:46 +0800

    Some changes

--- fullstackjava/chapter01/Case09MethodIntroduction.java
+++ fullstackjava/chapter01/Case09MethodIntroduction.java
@@ -3,11 +3,17 @@ package chapter01;
 public class Case09MethodIntroduction {
 
 	public static void main(String[] args) {
-		// method:
-		
-		// method definition:
-		
+
+
 		// method calling:
 	}
+
+	// method:
+	public int sum(int num1, int num2) {
+		return (num1 + num2);
+	}
+	
+	// method definition:
+	void say(String letters) {};
 
 }
