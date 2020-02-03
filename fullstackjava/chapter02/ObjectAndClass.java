cc3299b6146c0831c2828e2a68fb9821f907b296
FanHuanQing <huanqing19901001@163.com>
2020-02-03 21:05:49 +0800

    Add a new unit

---
+++ fullstackjava/chapter02/ObjectAndClass.java
@@ -0,0 +1,32 @@
+package chapter02;
+
+public class ObjectAndClass {
+
+	public static void main(String[] args) {
+		// Object oriented programming:
+		// 1.structure program design:
+		
+		// 2.abstract data type:
+		
+		// 3.class:
+		
+		
+		// Class definition:
+		// 1.member variable
+		
+		// 2.methods of class
+		
+		
+		// Create and use object:
+		// 1.use key word 'new' to create object
+		
+		// 2.reference variable:
+		
+		// 3.access member variable of object and call methods:
+		
+		// 4.value reference variable:
+		
+		// 5.Null and NullPiontException:
+	}
+
+}
