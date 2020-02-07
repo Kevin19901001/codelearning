4c5a95a020f60c3d2d04168d9feedb66d6df92c2
FanHuanQing <huanqing19901001@163.com>
2020-02-07 23:07:15 +0800

    New content about method reload

--- fullstackjava/chapter02/Case02MethodnadArray.java
+++ fullstackjava/chapter02/Case02MethodnadArray.java
@@ -4,6 +4,15 @@ public class Case02MethodnadArray {
 
 	public static void main(String[] args) {
 		
+		
+		
+		// Constructure:
+		
+		// Reference type array:
 	}
-
+	
+	// Method overload:
+	// Method sign: method name and argument list.
+	public boolean pay(double money) {return true;}
+	public boolean pay(String cardNo, String pwd) {return false;}
 }
