912969b6d3f51cc08998176a4aed95df429210a8
FanHuanQing <huanqing19901001@163.com>
2020-02-11 22:14:49 +0800

    Introduce JVM memory

--- fullstackjava/chapter02/Case03MemoryAndInheritence.java
+++ fullstackjava/chapter02/Case03MemoryAndInheritence.java
@@ -1,9 +1,31 @@
 package chapter02;
 
+import javax.swing.JFrame;
+
 public class Case03MemoryAndInheritence {
 
 	public static void main(String[] args) {
 		// 1.Obejct memory management:
+		// JVM memory: heap, stack, method area
+		// Objects in the heap;
+		// GC: Garbage Collection;
+		// System.gc();
+		
+		// Local variable in stack;
+
+		// Member variable and local variable:
+		// Local variable: defined in method, without default value, must be inited when defining;
+		// Existed int stack and was cleared after method calling.
+		
+		// Member variable: defined in class, out of method, with defaut value;
+		// existed in heap when create object of class.
+		
+		// Method area: store class info;
+		// Just one method:
+		JFrame jf1 = new JFrame();
+		JFrame jf2 = new JFrame();
+		jf1.setSize(200, 300);
+		jf2.setSize(400, 500);
 		
 		
 		// 2.Inheritance:
