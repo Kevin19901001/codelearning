70ba9a3c39ed89acc0334280243f2a74c1ee9ec2
FanHuanQing <huanqing19901001@163.com>
2020-02-15 21:06:17 +0800

    Some new comtent

--- fullstackjava/chapter02/Case03MemoryAndInheritence.java
+++ fullstackjava/chapter02/Case03MemoryAndInheritence.java
@@ -2,6 +2,13 @@ package chapter02;
 
 import javax.swing.JFrame;
 
+import utils.Foo;
+
+/**
+ * To talk about Java memory mamangement and inheritance:
+ * @author HuanQing
+ * @since 2020-02-14
+ */
 public class Case03MemoryAndInheritence {
 
 	public static void main(String[] args) {
@@ -20,13 +27,23 @@ public class Case03MemoryAndInheritence {
 		// Member variable: defined in class, out of method, with defaut value;
 		// existed in heap when create object of class.
 		
-		// Method area: store class info
+		// Method area: store class info;
 		// Just one method:
 		JFrame jf1 = new JFrame();
 		JFrame jf2 = new JFrame();
-		jf1.setSize(100, 300);
+		jf1.setSize(200, 300);
 		jf2.setSize(400, 500);
 		
+		
+		// Super reference refer to sub object:
+		Foo foo = new Goo(10);
+		
+		// Reference of super class can refer to object of sub class,
+		// but this reference can access members of super class only, 
+		// can not access those in sub class
+		// foo.num = 5;	// Syntax error!
+		// foo.g();		// Syntax error!
+		foo.f();		// method foo()
 	}
 
 }
@@ -41,3 +58,38 @@ class Animal {
 class Cat extends Animal {
 	public Cat() {}
 }
+
+/**
+ * Sub class extends supuer class,
+ * it has to add a constructor from super class,
+ * by method super(*args).
+ * @author HuanQing
+ * @since 2020-02-15
+ */
+class Goo extends Foo {
+
+	int num;
+	
+	public Goo(int value) {
+		super(value);
+	}
+	
+	public Goo(int value, int num) {
+		super(value);
+		this.num = num;
+	}
+	
+	public int getNum() {
+		return num;
+	}
+
+	public void setNum(int num) {
+		this.num = num;
+	}
+
+	public void g() {
+		System.out.println("method g()");
+	}
+	
+}
+
