05b37d195cb59a7c57a1988e2512926438b51254
FanHuanQing <huanqing19901001@163.com>
2020-03-20 20:41:40 +0800

    Some new content

--- fullstackjava/chapter03/Case03DateAndCollection.java
+++ fullstackjava/chapter03/Case03DateAndCollection.java
@@ -6,6 +6,10 @@ public class Case03DateAndCollection {
 		// 1. 日期操作
 		// 1.1 Date及其常用API
 		// 1.1.1 Java中的时间
+		// Java中的时间使用标准类库的Date类表示，是用一个距离固定时间点的毫秒数（可正可负，long类型）表达一个特定的时间点
+		
+		// 1.1.2 Date类简介
+		// java.util.Date类封装日期及时间信息
 	}
 
 }
