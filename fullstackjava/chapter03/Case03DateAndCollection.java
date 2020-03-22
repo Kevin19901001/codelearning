f52c2aab24c1d4b2d31f79ccecb53ad014c28eab
FanHuanQing <huanqing19901001@163.com>
2020-03-22 17:44:51 +0800

    Some new content

--- fullstackjava/chapter03/Case03DateAndCollection.java
+++ fullstackjava/chapter03/Case03DateAndCollection.java
@@ -1,6 +1,10 @@
 package chapter03;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+
+import org.junit.Test;
 
 public class Case03DateAndCollection {
 
@@ -19,6 +23,71 @@ public class Case03DateAndCollection {
 		long time = date.getTime();
 		System.out.println(time);
 		
+		
+		// 1.2 SimpleDateFormat
+		// 1.2.1 SimepleDateFormat简介
+		// java.text.SimpleDateFormat是一个以与语言环境有关的方式来格式化和解析日期的具体类。
+		// 构造方法：
+		// SimpleDateFormat(); SimpleDateFormat(String pattern)
+		// 方法：
+		// final String format(Date date) Date => String
+		// Date parse(String source) throw ParseException String => Date
+	}
+	
+	/**
+	 * 1.1.3 setTime()和getTime()方法
+	 */
+	@Test
+	public void testSetTime() {
+		Date date = new Date();
+		System.out.println(date);
+		
+		long time = date.getTime();
+		time += 60 * 60 * 24 * 1000;
+		
+		date.setTime(time);
+		System.out.println(date);
+	}
+	
+	/**
+	 * 1.1.4 Date重写toString()方法
+	 */
+	@Test
+	public void testDateToString() {
+		// Date重写了toString()方法，用一个字符串来描述当前Date对象所表示的时间
+		Date date = new Date();
+		System.out.println(date.toString());
+	}
+	
+	/**
+	 * 1.2.3 将Date转换为String
+	 */
+	@Test
+	public void testDateFormat() {
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd hh-mm-ss");
+		
+		Date date = new Date();
+		String formatedDate = format.format(date);
+		
+		System.out.println(formatedDate);
+	}
+	
+	/**
+	 * 1.2.4 将String解析为Date
+	 */
+	@Test
+	public void testFormat() {
+		String str = "1990-10-01";
+		
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+		Date date = new Date();
+		try {
+			date = format.parse(str);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println(date);
 	}
 
 }
